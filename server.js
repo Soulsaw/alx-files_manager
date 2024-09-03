f843e6889834f9d4625ffa34b4ccc702a1d0355e
Soulsaw <ssawadogo292@gmail.com>
2024-09-03 13:39:46 +0000

    fix the bug

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const router = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(router);
 app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server express is running on:${port}`);
